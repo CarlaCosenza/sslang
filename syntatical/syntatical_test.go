c8d33934696503c2b347da4dc64d9be9d75f1811
NataliaGodot <natalia_godot@hotmail.com>
2019-09-20 00:38:29 -0300

    Correct syntatical test to use implemented parse function

--- syntatical/syntatical_test.go
+++ syntatical/syntatical_test.go
@@ -4,45 +4,41 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/lucbarr/sslang/lexical"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRun(t *testing.T) {
 	tt := map[string]struct {
-		program string
-		err     error
+		tokens []int
+		err    error
 	}{
 		"test sample program syntactically correct": {
-			program: `
-			function main(arg:integer):integer
-			{
-				var a:integer;
-				var b:integer;
-				var c:integer;
-				b = 1;
-				c = 2;
-			}`,
+			tokens: []int{lexical.Function, lexical.ID, lexical.LeftParenthesis, lexical.ID,
+				lexical.Colon, lexical.Integer, lexical.RightParenthesis, lexical.Colon,
+				lexical.Integer, lexical.LeftBraces, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.ID, lexical.Equals, lexical.Numeral,
+				lexical.ID, lexical.Equals, lexical.Numeral, lexical.RightBraces, lexical.EOF},
 			err: nil,
 		},
 		"test sample program syntactically incorrect": {
-			program: `
-			function main(arg:integer):integer
-			{
-				var a:integer;
-				var b:integer
-				var c:integer;
-				b 1;
-				c = 2;
-			}`,
+			tokens: []int{lexical.Function, lexical.ID, lexical.LeftParenthesis, lexical.ID,
+				lexical.Colon, lexical.Integer, lexical.RightParenthesis, lexical.Colon,
+				lexical.Integer, lexical.LeftBraces, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.Var, lexical.ID, lexical.Colon,
+				lexical.Integer, lexical.Semicolon, lexical.ID, lexical.Equals, lexical.Numeral,
+				lexical.ID, lexical.Equals, lexical.Numeral, lexical.RightBraces, lexical.EOF},
 			err: fmt.Errorf("Syntax error"),
 		},
 	}
 
 	for name, table := range tt {
 		t.Run(name, func(t *testing.T) {
-			program := []byte(table.program)
-			syntatical := NewSyntatical(program)
-			tokens, err := syntatical.Run()
+			syntatical, _ := NewParser("action_table.csv")
+			err := syntatical.Run(table.tokens)
 			assert.Equal(t, table.err, err)
 		})
 	}
