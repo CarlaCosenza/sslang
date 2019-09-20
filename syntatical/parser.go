c810919e01429dc365271a18056703bb724998f8
Luciano Barreira <luc.s.barreira@gmail.com>
2019-09-20 00:08:08 -0300

    Add parser logic sketch

--- syntatical/parser.go
+++ syntatical/parser.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"io"
 	"os"
+	"strconv"
 )
 
 // Parser parses the program
@@ -53,3 +54,88 @@ func buildActionTableFromFile(file string) ([][]string, error) {
 
 	return actionTable, nil
 }
+
+// Run runs the lexical analysis
+func (p *Parser) Run(tokens []int) error {
+	state := 0
+	iToken := 0
+	currentToken := tokens[iToken]
+
+	nextToken := func() {
+		iToken++
+		currentToken = tokens[iToken]
+	}
+
+	action := p.actionTable[state][TokenToAction[currentToken]]
+
+	for {
+
+		if accept(action) {
+			break
+		}
+
+		state, ok := shift(action)
+		if ok {
+			p.stateStack = append(p.stateStack, state)
+
+			nextToken()
+			action = p.actionTable[state][currentToken]
+
+			continue
+		}
+
+		state, ok = reduce(action)
+		if ok {
+			// TODO: reduce logic
+		}
+	}
+
+	return nil
+}
+
+func accept(s string) bool {
+	return s == "acc"
+}
+
+func reduce(s string) (int, bool) {
+	if len(s) == 0 {
+		return -1, false
+	}
+
+	if s[0] != 'r' {
+		return -1, false
+	}
+
+	n, err := strconv.Atoi(s[1:])
+
+	if err != nil {
+		return -1, false
+	}
+
+	return n, true
+}
+
+func shift(s string) (int, bool) {
+	if len(s) == 0 {
+		return -1, false
+	}
+
+	if s[0] != 's' {
+		return -1, false
+	}
+
+	n, err := strconv.Atoi(s[1:])
+
+	if err != nil {
+		return -1, false
+	}
+
+	return n, true
+}
+
+// Token parsing states
+const (
+	Shift = iota
+	Reduction
+	Accept
+)
